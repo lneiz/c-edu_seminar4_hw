434fcca4b7ce6e50be1b89321ffaacfb6ed0bae8
lneiz <yuntsevich@ymail.com>
2023-02-12 03:38:34 +0500

    #25 - done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// string GetPow(int a, int b)
+// {
+//     int result = a;
+//     for(int i = 0; i < (b - 1); i++)
+//     {
+//         result = result * a;
+//     }
+
+//     return Convert.ToString(result);
+// }
+
+// Console.Write("Введите число (A): ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("В какую степерь возвести число (B)? ");
+// int numB = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write(GetPow(numA, numB));
